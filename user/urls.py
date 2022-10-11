053c4eb0747ef38b02e6c6413a3f896342a0d99b
unknown <zisan321@gmail.com>
2022-10-11 23:34:09 +0600

    login logout functionality and homepage added

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from user import views
 
 urlpatterns = [
-    path('',views.firstPage, name="firstPage"),
-    path('profile/', views.profile, name='profile')
+    path('',views.home, name="home"),
+    path('signup/',views.signUp, name="signup"),
+    path('logout/',views.logOut, name='logout'),
+    path('login/', views.logIn, name='login'),
+    path('profile/', views.profile, name='profile'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login,logout, authenticate
 # Create your views here.
-def firstPage(request):
+def home(request):
+    return render(request, 'user/home.html')
+def signUp(request):
     if request.method=="GET":
-        return render(request, 'user/firstPage.html', {'form' : UserCreationForm()})
+        return render(request, 'user/signUp.html', {'form' : UserCreationForm()})
     else:
         try:
             if request.POST['password1'] == request.POST['password2']:
@@ -15,9 +17,22 @@ def firstPage(request):
                 login(request, new_user)
                 return redirect('profile')
         except IntegrityError:
-             return render(request, 'user/firstPage.html', {'form' : UserCreationForm(), 'error': "This username is already taken. Please try a new username."})
+             return render(request, 'user/signUp.html', {'form' : UserCreationForm(), 'error': "This username is already taken. Please try a new username."})
 
         else:
-            return render(request, 'user/firstPage.html', {'form' : UserCreationForm(), 'error': "Passwords did not match. Please input the passwords correctly."})
+            return render(request, 'user/signUp.html', {'form' : UserCreationForm(), 'error': "Passwords did not match. Please input the passwords correctly."})
+def logIn(request):
+    if request.method=="GET":
+        return render(request, 'user/login.html', {'form' : AuthenticationForm()})
+    else:
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render(request, 'user/login.html', {'form' : AuthenticationForm(), 'error': 'Username and password did not match.'})
+        else:
+            login(request, user)
+            return redirect('profile')
+def logOut(request):
+    logout(request)
+    return redirect('home')
 def profile(request):
     return render(request, 'user/profile.html')
