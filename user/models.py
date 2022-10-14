91803b0efd2a3ee6081c0ffd79f3445bca7f970b
Mehrab Islam Arnab <aarnabofficial@gmail.com>
2022-10-14 14:07:02 +0600

    Update models.py

--- user/models.py
+++ user/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
-
-# Create your models here.
+ 
+ 
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=256)
+ 
+ 
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    image = models.ImageField()
+    bump = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    tag = models.ManyToManyField(
+        Tag,
+    )
+ 
+ 
+class Comments(models.Model):
+    text = models.CharField(max_length=256)
+    image = models.ImageField()
+    bump = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    
