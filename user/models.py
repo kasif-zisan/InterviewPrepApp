359808f724fe5c30eb4aad3b23c12f5ccf81c2b4
God-Serenaa <aarnabofficial@gmail.com>
2022-11-11 06:00:40 +0600

    image models and directory added

--- user/models.py
+++ user/models.py
@@ -1,10 +1,6 @@
-from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
 from django.utils import timezone
-from django.db.models.signals import post_save
-from django.dispatch import receiver
  
 class Tag(models.Model):
     tag_name = models.CharField(max_length=256)
@@ -22,15 +18,18 @@ class Post(models.Model):
         Tag, related_name = 'post', blank=True
     )'''
 
- 
- 
+
+class PostImage(models.Model):
+    parent = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='post/image')
+
+
 class Comments(models.Model):
     text = models.CharField(max_length=256)
     bump = models.IntegerField()
     time = models.DateTimeField(default=timezone.now)
-    '''author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
-    )'''
     author = models.ForeignKey(
         User, on_delete=models.CASCADE
     )
@@ -38,10 +37,23 @@ class Comments(models.Model):
         Post, on_delete=models.CASCADE
     )
 
+
+class CommentImage(models.Model):
+    parent = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='comment/image')
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete= models.CASCADE)
     name = models.CharField(max_length=256, default="<anon>")
     works_at = models.CharField(max_length=256)
 
 
+class UserImage(models.Model):
+    parent = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='user/image')
     
