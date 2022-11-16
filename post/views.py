7870558cabfbd7c4c3fd51a94c28c6e7fbf8c462
God-Serenaa <aarnabofficial@gmail.com>
2022-11-16 06:28:34 +0600

    search and sort added

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from post.models import Post, Comment
 from rest_framework.generics import CreateAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import PostSerializer, CommentSerializer
 
 class NewPost(CreateAPIView):
@@ -10,9 +11,14 @@ class NewPost(CreateAPIView):
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'text', 'author__username']
+    ordering_fields = ['bump', 'date']
 
 class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['bump', 'date']
 
     def get_queryset(self):
         return Comment.objects.filter(parent_id=self.kwargs['post_pk'])
